d37f526ac320d37efc6461ad0aa7be002e73cfc9
NyHagan <nanaohagan@gmail.com>
2022-03-23 21:10:27 +0000

    add code for importing pymongo and dnspython in app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, url_for,request, render_template, redirect,flash, session )
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId    
 if os.path.exists("env.py"):
     import env
 

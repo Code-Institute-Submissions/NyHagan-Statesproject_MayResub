ed5244e330836fafc918e6acf1ed1d61a3938101
NyHagan <nanaohagan@gmail.com>
2022-03-27 19:00:08 +0000

    add profile page after sign in

--- app.py
+++ app.py
@@ -44,7 +44,9 @@ def create():
 
         
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
+        flash("Your account was created successfully!")
+        return redirect(url_for("profile", username=session["user"]))
+        
     return render_template("create.html")
 
 
@@ -59,6 +61,8 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for(
+                            "profile", username=session["user"]))
 
             else:
                 flash("Incorrect Username and/or Password")
@@ -69,6 +73,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.people.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":

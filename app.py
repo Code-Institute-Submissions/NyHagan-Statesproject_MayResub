1da7920cce761cc392556b32ec07c63514b80b8d
NyHagan <nanaohagan@gmail.com>
2022-04-03 20:05:18 +0000

    set debug to off in app.py

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def delfact():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)

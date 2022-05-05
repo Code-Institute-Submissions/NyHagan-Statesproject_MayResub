72fb5f89ced4be6e1f95ca5945a0e5354345bec0
NyHagan <nanaohagan@gmail.com>
2022-05-05 17:52:39 +0000

    format python code to be pep 8 compliant

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os #Allows flask and other elements to function
+# Allows flask and other elements to function
+import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -18,17 +19,19 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route("/")#function to grab the documents from mongo and display on names.html
+# function to grab the documents from mongo and display on names.html
+@app.route("/")
 @app.route("/get_names")
 def get_names():
     names = mongo.db.names.find()
     return render_template("names.html", names=names)
 
 
-@app.route("/create", methods=["GET", "POST"])#allows for account creation
+# allows for account creation
+@app.route("/create", methods=["GET", "POST"])
 def create():
     if request.method == "POST":
-        
+
         existing_user = mongo.db.people.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -42,7 +45,6 @@ def create():
         }
         mongo.db.people.insert_one(create)
 
-        
         session["user"] = request.form.get("username").lower()
         flash("Your account was created successfully!")
         return redirect(url_for("profile", username=session["user"]))
@@ -50,15 +52,16 @@ def create():
     return render_template("create.html")
 
 
-@app.route("/login", methods=["GET", "POST"])#authentication to allow logging in
+# authentication to allow logging in
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if  request.method == "POST":
+    if request.method == "POST":
         existing_user = mongo.db.people.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                    existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
@@ -79,7 +82,7 @@ def login():
 def profile(username):
     username = mongo.db.people.find_one(
         {"username": session["user"]})["username"]
-    
+
     if session["user"]:
         return render_template("profile.html", username=username)
 
@@ -91,44 +94,46 @@ def logout():
     session.clear()
     return redirect(url_for("get_names"))
 
-#allows user to enter a fact and send it off to mongodb
-@app.route("/fact", methods=["GET", "POST"]) 
+
+# allows user to enter a fact and send it off to mongodb
+@app.route("/fact", methods=["GET", "POST"])
 def fact():
-    if  request.method == "POST":
-        
+    if request.method == "POST":
+
         find_state = request.form.get("find_state").capitalize()
 
         new_fact = request.form.get("add_fact").capitalize()
 
         if mongo.db.names.find_one({"state_name": find_state}):
 
-            mongo.db.names.find_one_and_update({"state_name": find_state}, 
-                                 {"$set": {"fun_fact": new_fact}} 
-                                 )
+            mongo.db.names.find_one_and_update({"state_name": find_state},
+                                               {"$set": {"fun_fact": new_fact}}
+                                               )
 
             flash("A new Fact Has been Added!")
 
         else:
             flash("There is no state called " + find_state)
-    
+
     return render_template("fact.html")
 
 
-@app.route("/delfact", methods=["GET", "POST"])#delete a fact via this function
+# delete a fact via this function
+@app.route("/delfact", methods=["GET", "POST"])
 def delfact():
-    if  request.method == "POST":
-        
+    if request.method == "POST":
+
         del_state = request.form.get("del_state").capitalize()
 
-        mongo.db.names.find_one_and_update({"state_name": del_state}, 
-                                 {"$set": {"fun_fact":''}} 
-                                 )
+        mongo.db.names.find_one_and_update({"state_name": del_state},
+                                           {"$set": {"fun_fact": ''}}
+                                           )
         flash("Fact has been Deleted")
         return redirect(url_for('fact'))
         print(del_state)
 
     else:
-        return render_template("delfact.html")    
+        return render_template("delfact.html")
 
 
 if __name__ == "__main__":
